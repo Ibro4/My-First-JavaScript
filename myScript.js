7ed8884e9f13179108ca35f39429c74b9b09842a
Ibro4 <ibrahim.a868@slcstudents.org>
2018-12-20 08:27:44 -0700

    added java code

---
+++ myScript.js
@@ -0,0 +1,13 @@
+function myFunction () {
+    document.getElementById('demo').innerHTML = "paragraph changed";
+}
+
+//Initializing Variables
+
+var x = 5;
+var y = 6;
+var z = x + y;
+
+function myMathFunction() {
+    document.getElementById('math').innerHTML= z;
+}
